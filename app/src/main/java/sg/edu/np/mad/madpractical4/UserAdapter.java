644ba251a0bc6c6f1b9a35a20c06d1bf24eeb784
Isa-AcTiEw <s10257760@gmail.com>
2024-05-13 18:51:51 +0800

    created the layout for the recycler view and implemented the respective viewHolder and Adapter

---
+++ app/src/main/java/sg/edu/np/mad/madpractical4/UserAdapter.java
@@ -0,0 +1,25 @@
+package sg.edu.np.mad.madpractical4;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserAdapter extends RecyclerView.Adapter <UserViewHolder> {
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
